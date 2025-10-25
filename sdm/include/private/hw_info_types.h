605e548c9ca9df464e1279c4f239c322e8d09c51
Fazil Sheik <fazilsheik@aospa.co>
2025-10-25 07:47:22 +0530

    display-core: Make DS Merge Node configurable
    Not supported by OnePlus SM8750 blobs.
    
    Change-Id: I7244e6139dd08951f6be99f16c20f2cb2202649a
    Signed-off-by: Fazil Sheik <fazilsheik@aospa.co>

--- sdm/include/private/hw_info_types.h
+++ sdm/include/private/hw_info_types.h
@@ -788,11 +788,13 @@ struct HWScaleData {
   uint32_t cac_re_asym_phase_step_v = 0;
 };
 
+#ifndef DS_MERGE_NODE_UNSUPPORTED
 enum DSMergeMode {
   kDestScalerSinglePipe,
   kDestScalerDualPipe,
   kDestScalerQuadPipe,
 };
+#endif
 
 struct HWDestScaleInfo {
   uint32_t mixer_width = 0;
@@ -800,7 +802,9 @@ struct HWDestScaleInfo {
   bool scale_update = false;
   HWScaleData scale_data = {};
   LayerRect panel_roi = {};
+#ifndef DS_MERGE_NODE_UNSUPPORTED
   DSMergeMode mixer_merge_mode = kDestScalerSinglePipe;
+#endif
 };
 
 typedef std::map<uint32_t, HWDestScaleInfo *> DestScaleInfoMap;
